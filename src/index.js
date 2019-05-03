fdcfaaeb07dc3486e40a7ed062e31244a6a8e3a4
Gilberto <savoini@dac246.dac.unicamp.br>
2019-05-03 08:27:04 -0300

    Primeiro componente React

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-alert("10");
+import React from "react";
+import { render } from "react-dom";
+
+class Button extends React.Component {
+  render() {
+    return <a href="">Salvar</a>;
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello</h1>
+        <Button />
+      </React.Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById("app"));
